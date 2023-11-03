04cbe61d4a6ae421e570c015efcf1be6d34cc8d6
jankorczak <jasiukorczak@gmail.com>
2023-11-04 00:29:03 +0100

    Working on saving 3 min video left

--- index.php
+++ index.php
@@ -14,9 +14,7 @@ require_once 'includes/signup_view.inc.php';
     <h3>Signup</h3>
 
     <form action="includes/signup.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="password" placeholder="Password">
-        <input type="text" name="email" placeholder="E-mail">
+        
         <button>Sign up</button>
     </form>
 
