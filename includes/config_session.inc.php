7669eed03e66a9b57ed18af3f95b151df37c4739
jankorczak <jasiukorczak@gmail.com>
2023-11-04 23:44:39 +0100

    33:20 on tut

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -15,20 +15,47 @@ session_set_cookie_params([
 
 session_start();
 
+if (isset($_SESSION["user_id"])) {
+    if (!isset($_SESSION["last_regeneration"])) {
+        regenerate_session_id_loggedin();
+    } else {
+        // after 30 mins regenerate again using if statement
+    
+        $interval = 60 * 30;
+        if(time() -  $_SESSION["last_regeneration"] >= $interval) {
+            regenerate_session_id_loggedin();
+        }
+    }
+    
+} else {
+    if (!isset($_SESSION["last_regeneration"])) {
+        regenerate_session_id();
+    } else {
+        // after 30 mins regenerate again using if statement
+    
+        $interval = 60 * 30;
+        if(time() -  $_SESSION["last_regeneration"] >= $interval) {
+            regenerate_session_id();
+        }
+    }
+    
+}
+
 // if not regenereated make it regenerate
 
-if (!isset($_SESSION["last_regeneration"])) {
-    regenerate_session_id();
-} else {
-    // after 30 mins regenerate again using if statement
+function regenerate_session_id_loggedin() {
+    
+    session_regenerate_id(true);
 
-    $interval = 60 * 30;
-    if(time() -  $_SESSION["last_regeneration"] >= $interval) {
-        regenerate_session_id();
-    }
+    $userId = $_SESSION["user_id"];
+    $newSessionId = session_create_id();
+    $sessionId = $newSessionId . "_" . $userId;
+    session_id($sessionId);
+
+    $_SESSION["last_regeneration"] = time();
 }
 
 function regenerate_session_id() {
-    session_regenerate_id();
+    session_regenerate_id(true);
     $_SESSION["last_regeneration"] = time();
 }
--- includes/login.inc.php
+++ includes/login.inc.php
@@ -12,17 +12,18 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         // ERROR HANDLERS
         $errors = [];
 
-        if (is_input_empty($username, $password, $email)) {
+        if (is_input_empty($username, $pwd)) {
             $errors["empty_input"] = "Fill in all fields!";
         }
-        if (is_email_invalid($email)) {
-            $errors["invalid_email"] = "Invalid email used!";
+
+        $result = get_user($pdo, $username);
+
+        if (is_username_wrong($result)) {
+            $errors["login_incorrect"] = "Incorrect login info!";
         }
-        if (is_username_taken($pdo, $username)) {
-            $errors["username_taken"] = "Username already taken!";
-        }
-        if (is_email_registered($pdo, $email)) {
-            $errors["email_used"] = "Email already registered!";
+
+        if (!is_username_wrong($result) && is_password_wrong($pwd, $result["pwd"])) {
+            $errors["login_incorrect"] = "Incorrect login info!";
         }
 
         require_once 'config_session.inc.php';
@@ -30,16 +31,25 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         if ($errors) {
             $_SESSION["errors_signup"] = $errors;
 
-            $signupData = [
-                "username" => $username,
-                "email" => $email
-            ];
-            $_SESSION["signup_data"] = $signupData;
-
             header("Location: ../index.php");
             die();
         } 
-    } catch (\Throwable $th) {
+
+        $newSessionId = session_create_id();
+        $sessionId = $newSessionId . "_" . $result["id"];
+        session_id($sessionId);
+
+        $_SESSION["user_id"] = $result["id"];
+        $_SESSION["user_username"] = htmlspecialchars($result["username"]);
+
+        $_SESSION["last_regeneration"] = time();
+
+        header("Location: ../index.php?login=success");
+        $pdo = null;
+        $stmt = null;
+
+        die();
+    } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
     }
 } else {
